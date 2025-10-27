329797de32fe947a8dfd08e7ed9c1c479d6c03d7
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-10-27 21:55:05 +0530

    added functionalities upon clicking

--- lightbulb/script.js
+++ lightbulb/script.js
@@ -2,3 +2,12 @@
 let bulb = document.getElementById("bulb");
 let onBtn = document.getElementById("onBtn");
 let offBtn = document.getElementById("offBtn");
+
+
+onBtn.addEventListener("click", () => {
+    bulb.src = "https://www.w3schools.com/js/pic_bulbon.gif";
+});
+
+offBtn.addEventListener("click", () => {
+    bulb.src = "https://www.w3schools.com/js/pic_bulboff.gif";
+});

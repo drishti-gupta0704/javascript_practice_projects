5997390e98de97658e8cb9e038c534bdb623b6e7
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-10-30 22:09:15 +0530

    selecting elements through JS

---
+++ form_validation/script.js
@@ -0,0 +1,12 @@
+
+const form = document.getElementById('signupForm');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirmPassword');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); 
+  
+});
+

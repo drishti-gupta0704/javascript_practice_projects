eca936412473eef9724620f696ae4f3e8385db72
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-11-10 13:52:27 +0530

    adding the JS logic to fetch API..used github API endpoint to extract user  details

--- fetch_github_user/script.js
+++ fetch_github_user/script.js
@@ -1,1 +1,26 @@
 
+const searchbtn= document.querySelector("#searchbtn");
+const result = document.querySelector("#result");
+
+searchbtn.addEventListener("click",async()=>{
+const username = document.querySelector("#username").value.trim();
+if (!username) {
+        result.innerText = "Please enter a username";
+        return;
+    }
+
+result.innerText= "loading ....";
+const response = await fetch(`https://api.github.com/users/${username}`);
+const data = await response.json();
+console.log(data);
+
+result.innerHTML= `<img src= " ${data.avatar_url}"  alt= avatar>
+                   <p>Name: ${data.name || "No name provided"}</p>
+                   <p>Followers: ${data.followers}</p>
+                  <p>Public Repos: ${data.public_repos}</p> `;
+
+
+
+})
+
+

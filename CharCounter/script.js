f467d26b2957494ec7e7df6f7b8dd04098553f2e
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-11-17 20:36:52 +0530

    adding JS to create progress bar

--- CharCounter/script.js
+++ CharCounter/script.js
@@ -1,16 +1,17 @@
 
 
-
 const textBox = document.getElementById("textBox");
 const count = document.getElementById("count");
+const progressBar = document.getElementById("progressBar");
 
-
-
+const maxChars = 100;
 
 textBox.addEventListener("input", () => {
   const typedLength = textBox.value.length;
   count.textContent = typedLength;
 
 
- 
+  let progressPercent = (typedLength / maxChars) * 100;
+  if(progressPercent > 100) progressPercent = 100; 
+  progressBar.style.width = progressPercent + "%";
 });
